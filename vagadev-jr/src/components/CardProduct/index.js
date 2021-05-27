118dae875c4a46d144b3c716e78659073117f8de
Alisson Diogo <38612359+AlissonDiogo@users.noreply.github.com>
2021-05-26 21:57:17 -0300

    added maskmoney

--- vagadev-jr/src/components/CardProduct/index.js
+++ vagadev-jr/src/components/CardProduct/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Tooltip from "../Tooltip";
-
+import { toMoney } from "vanilla-masker";
 import "./cardProduct.css";
 
 function CardProduct(props) {
-  
   return (
     <div className="card-container">
       <div className="card-image">
-        <img src={props.image} />
+        <img src={props.image} alt={"Imagem do produto: " + props.title} />
       </div>
       <div className="card-bottom">
         <h3 className="product-title">
@@ -18,7 +17,10 @@ function CardProduct(props) {
             props.title
           )}
         </h3>
-        <div className="product-price">{"R$ " + props.price}</div>
+        <div className="product-price">
+          {"R$ " + toMoney(props.price.toFixed(2))}
+          {/* {"R$ " + toMoney(123)} */}
+        </div>
         <div className="container-buy-button">
           <button className="buy-button">COMPRAR</button>
         </div>
